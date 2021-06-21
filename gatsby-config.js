27e593e4d68dac27fb1ef7b97972f4562d55e387
bsplt <a.lehmann@protonmail.com>
2021-06-21 10:10:38 +0200

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteMetadata: {
+    title: "Backside",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/assets/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
---
+++ src/components/layout.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./layout.css";
+import "/node_modules/modern-normalize/modern-normalize.css";
+
+export default function Layout({ children }) {
+  return <div>{children}</div>;
+}
