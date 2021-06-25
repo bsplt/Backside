a35f01af6244ccf90c49f89b2c5e65333bcca980
bsplt <a.lehmann@protonmail.com>
2021-06-25 08:21:17 +0200

    integrated navigation

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "/node_modules/modern-normalize/modern-normalize.css";
 import "./layout.css";
+import Navigation from "./navigation.js";
+import Animation from "./navAnimation.js";
 
 export default function Layout({ children }) {
-  return <div className="layout">{children}</div>;
+  const animation = Animation();
+  const navigation = [
+    { page: "/", content: animation, renderAsBlocks: true },
+    { page: "/", content: "Projects" },
+    { page: "/about", content: "About" },
+  ];
+
+  return (
+    <div className="layout">
+      <Navigation navigation={navigation} />
+      {children}
+    </div>
+  );
 }
