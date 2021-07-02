a03f9ccd2424894ff87ec6471b39680b1d4f4698
Alexander Lehmann <a.lehmann@protonmail.com>
2021-07-02 17:08:45 +0200

    remove placeholder code

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,39 +4,7 @@ import Layout from "../components/layout.js";
 const IndexPage = () => {
   return (
     <Layout style="index">
-      <aside>
-        &#x2593;&#x2593;&#x2593;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x2591;&#x2591;&#x0070;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x2591;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#x2591;&#x2591;&#x1FB90;&#x1FB90;&#x1FB90;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;
-        <br />
-        &#x2591;&#x2591;&#x2591;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x2591;&#x2591;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x2593;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#x2591;&#x2591;&#x2593;&#x2593;&#x2593;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;
-        <br />
-        &#x2591;&#x2591;&#x2591;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x2593;&#x2593;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x2591;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2591;&#x2591;&nbsp;&nbsp;&#x0074;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;
-        <br />
-        &#x2591;&#x2591;&#x2591;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2593;&#x2591;&#x2591;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&#x2593;&#x2593;&#x2593;&#x2593;&#x2591;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x2593;&#x2593;&nbsp;&nbsp;&nbsp;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;
-        <br />
-        &#x2591;&#x2591;&#x2591;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#x0073;&#x2591;&#x2591;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&nbsp;&nbsp;&nbsp;&#x006C;&#x2591;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x1FB90;&#x2591;&#x2591;&nbsp;&nbsp;&nbsp;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;&#x2591;
-      </aside>
-      <main>
-        <p>
-          But I must explain to you how all this mistaken idea of denouncing
-          pleasure and praising pain was born and I will give you a complete
-          account of the system, and expound the actual teachings of the great
-          explorer of the truth, the master-builder of human happiness. No one
-          rejects, dislikes, or avoids pleasure itself, because it is pleasure,
-          but because those who do not know how to pursue pleasure rationally
-          encounter consequences that are extremely painful. Nor again is there
-          anyone who loves or pursues or desires to obtain pain of itself,
-          because it is pain, but because occasionally circumstances occur in
-          which toil and pain can procure him some great pleasure. To take a
-          trivial example, which of us ever undertakes laborious physical
-          exercise, except to obtain some advantage from it? But who has any
-          right to find fault with a man who chooses to enjoy a pleasure that
-          has no annoying consequences, or one who avoids a pain that produces
-          no resultant pleasure?
-        </p>
-        <p>
-          ....v....1....v....2....v....3....v....4....v....5....v....6....v....7
-        </p>
-      </main>
+      <Graphic width={GetPageWidth()} />
     </Layout>
   );
 };
