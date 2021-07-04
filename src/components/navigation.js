f3119e250216131c9265e3711ee6c805007ed921
Alexander Lehmann <a.lehmann@protonmail.com>
2021-07-04 22:55:23 +0200

    clean up navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,21 +6,9 @@ export default function Navigation(props) {
   const navigation = props.navigation;
   const links = navigation.map((link) => (
     <span className={link.renderAsBlocks ? "blocks" : ""}>
-      <Link to={link.page}>{link.content}</Link>
+      <Link to={link.page}>{link.content}</Link>&nbsp;
     </span>
   ));
 
-  const fillChar = "\u2591";
-  let fill = "";
-  for (let i = 0; i < 60; i++) {
-    fill += fillChar;
-  }
-
-  return (
-    <div className="navigation">
-      {/*<span className="blocks">{fillChar}{fillChar}&nbsp;</span>*/}
-      {links}
-      <span className="blocks">{fill}</span>
-    </div>
-  );
+  return <div className="navigation">{links}</div>;
 }
