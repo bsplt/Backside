84949088a6cd0c30e6f051ee0ccb457694899d54
Alexander Lehmann <a.lehmann@protonmail.com>
2021-07-10 15:05:58 +0200

    add description component for testing

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,12 @@ import * as React from "react";
 import Layout from "../components/layout.js";
 import "./about.css";
 import GetPageSize from "../components/getPageSize";
+import Description from "../components/Description.js";
 
 const AboutPage = () => {
   return (
     <Layout style="about">
+      <Description title="test" date="7.7.2021" />
       <main>
         <p>
           But I must explain to you how all this mistaken idea of denouncing
@@ -25,6 +27,11 @@ const AboutPage = () => {
           no resultant pleasure?
         </p>
         <p>{GetPageSize()}</p>
+        <p>Offener Brief für flutschende Ligaturen finden</p>
+        <p>or one who avoids a pain that produces no resultant</p>
+        <p className="blocks">
+          or one who avoids a pain that produces no resultant
+        </p>
         <p>
           But I must explain to you how all this mistaken idea of denouncing
           pleasure and praising pain was born and I will give you a complete
@@ -43,7 +50,11 @@ const AboutPage = () => {
           no resultant pleasure?
         </p>
         <p>
-          ....v....1....v....2....v....3....v....4....v....5....v....6....v....7
+          ....v....1....v....2....v....3....v....4....v....5....v....6....v....7....
+          <span className="blocks">|</span>
+        </p>
+        <p className="blocks">
+          ....v....1....v....2....v....3....v....4....v....5....v....6....v....7....|
         </p>
       </main>
     </Layout>
